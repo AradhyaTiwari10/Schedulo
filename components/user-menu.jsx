53b26af9f3983b9d4f3f6087cc0a429e9d1147e5
AradhyaTiwari10 <aradhyafcb@gmail.com>
2025-05-02 22:18:46 +0530

    page.jsx added testimonials carausels and full dashboard UI

--- components/user-menu.jsx
+++ components/user-menu.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+"use client";
+
+import { UserButton } from "@clerk/nextjs";
+import { ChartNoAxesGantt } from "lucide-react";
 
 const UserMenu = () => {
   return (
-    <div>
-        Usermenu
-    </div>
-  )
-}
+    <UserButton
+      appearance={{
+        elements: {
+          avatarBox: "w-10 h-10",
+        },
+      }}
+    >
+      <UserButton.MenuItems>
+        <UserButton.Link
+          label="My Events"
+          labelIcon={<ChartNoAxesGantt size={15} />}
+          href="/events"
+        />
+        <UserButton.Action label="manageAccount" />
+      </UserButton.MenuItems>
+    </UserButton>
+  );
+};
 
-export default UserMenu
+export default UserMenu;
